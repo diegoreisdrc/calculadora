ab9f8cd06d67b06b0c85408be30a6dfd0a22c2b2
DRC <diegoreisdr059@gmail.com>
2018-08-21 07:36:01 -0300

    Tudo quase pronto

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,15 +1,34 @@
-function calcula() {
+function divisor() {
 	var  valor1 = parseInt(document.getElementById("valor1").value);
 	var  valor2 = parseInt(document.getElementById("valor2").value);
 	var divisor = document.getElementById("divisor");
-	var multiplicador = document.getElementById("multiplicador");
-	var adicao = document.getElementById("adicao").value;
-	var subtracao = document.getElementById("subtracao");
 
-	var calculo = valor1++valor2;
+	var calculo = valor1 / valor2;
 	alert(calculo);
 }
+function multiplicador() {
+	var  valor1 = parseInt(document.getElementById("valor1").value);
+	var  valor2 = parseInt(document.getElementById("valor2").value);
+	var multiplicador = document.getElementById("multiplicador");
 
+	var calculo = valor1 * valor2;
+		alert(calculo);
+}
+function adicao() {
+	var  valor1 = parseInt(document.getElementById("valor1").value);
+	var  valor2 = parseInt(document.getElementById("valor2").value);
+	var adicao = document.getElementById("adicao");
 
+	var calculo = valor1 + valor2;
+	alert(calculo);
+}
+function subtracao() {
+	var  valor1 = parseInt(document.getElementById("valor1").value);
+	var  valor2 = parseInt(document.getElementById("valor2").value);
+	var adicao = document.getElementById("subtracao");
+
+	var calculo = valor1 - valor2;
+	alert(calculo);
+}
 
 
