b4966a09a0f4cff94f831b7401b2494d125737a8
DRC <diegoreisdr059@gmail.com>
2018-08-20 21:52:48 -0300

    Primeiras funções

---
+++ js/funcoes.js
@@ -0,0 +1,15 @@
+function calcula() {
+	var  valor1 = parseInt(document.getElementById("valor1").value);
+	var  valor2 = parseInt(document.getElementById("valor2").value);
+	var divisor = document.getElementById("divisor");
+	var multiplicador = document.getElementById("multiplicador");
+	var adicao = document.getElementById("adicao").value;
+	var subtracao = document.getElementById("subtracao");
+
+	var calculo = valor1++valor2;
+	alert(calculo);
+}
+
+
+
+
